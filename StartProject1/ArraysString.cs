8b56c414aa4a3016880613b940c0ddee36f38d0d
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 15:18:19 +0545

    added

--- StartProject1/ArraysString.cs
+++ StartProject1/ArraysString.cs
@@ -1,29 +1,29 @@
-﻿using System;
+﻿//using System;
 
-public class Program
-{
-    static void Main(string[] args)
-    {
-        // Example of an array
-        int[] numbers = { 1, 2, 3, 4, 5 };
+//public class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Example of an array
+//        int[] numbers = { 1, 2, 3, 4, 5 };
 
-        // Example of a for loop to iterate through the array and print each element
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            Console.WriteLine(numbers[i]);
-        }
-        Console.WriteLine("\n");
+//        // Example of a for loop to iterate through the array and print each element
+//        for (int i = 0; i < numbers.Length; i++)
+//        {
+//            Console.WriteLine(numbers[i]);
+//        }
+//        Console.WriteLine("\n");
 
-        // Example of a string
-        string greeting = "Hello, world!";
+//        // Example of a string
+//        string greeting = "Hello, world!";
 
-        // Example of accessing individual characters of a string using the index operator
-        Console.WriteLine(greeting[0]); // Output: H
+//        // Example of accessing individual characters of a string using the index operator
+//        Console.WriteLine(greeting[0]); // Output: H
 
-        // Example of creating a new string based on an existing string using the Replace method
-        string newGreeting = greeting.Replace("Hello", "Hi");
-        Console.WriteLine(newGreeting); // Output: Hi, world!
+//        // Example of creating a new string based on an existing string using the Replace method
+//        string newGreeting = greeting.Replace("Hello", "Hi");
+//        Console.WriteLine(newGreeting); // Output: Hi, world!
 
-        Console.ReadLine(); // Pause the console to see the output
-    }
-}
+//        Console.ReadLine(); // Pause the console to see the output
+//    }
+//}
