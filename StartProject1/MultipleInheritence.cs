f02ce2faae238d78256c101a6af7487893db7134
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 15:17:35 +0545

    add: Multiple Inheritence

---
+++ StartProject1/MultipleInheritence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+interface IShape
+{
+    void Draw();
+}
+
+interface IColor
+{
+    void Colorize();
+}
+
+class Circle : IShape, IColor
+{
+    public void Draw()
+    {
+        Console.WriteLine("Circle is drawn.");
+    }
+
+    public void Colorize()
+    {
+        Console.WriteLine("Circle is colorized.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle();
+        circle.Draw();
+        circle.Colorize();
+        Console.ReadLine();
+    }
+}
