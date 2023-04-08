3c68d583faad06754bd3cee1b63b0ed43de8bc7c
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 16:50:33 +0545

    extras

--- StartProject1/Abstract.cs
+++ StartProject1/Abstract.cs
@@ -1,44 +1,44 @@
-﻿using System;
+﻿//using System;
 
-public abstract class Shape
-{
-    public abstract double CalculateArea();
-}
+//public abstract class Shape
+//{
+//    public abstract double CalculateArea();
+//}
 
-public class Rectangle : Shape
-{
-    public double Width { get; set; }
-    public double Height { get; set; }
+//public class Rectangle : Shape
+//{
+//    public double Width { get; set; }
+//    public double Height { get; set; }
 
-    public override double CalculateArea()
-    {
-        return Width * Height;
-    }
-}
+//    public override double CalculateArea()
+//    {
+//        return Width * Height;
+//    }
+//}
 
-public class Circle : Shape
-{
-    public double Radius { get; set; }
+//public class Circle : Shape
+//{
+//    public double Radius { get; set; }
 
-    public override double CalculateArea()
-    {
-        return Math.PI * Math.Pow(Radius, 2);
-    }
-}
+//    public override double CalculateArea()
+//    {
+//        return Math.PI * Math.Pow(Radius, 2);
+//    }
+//}
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Rectangle rectangle = new Rectangle();
-        rectangle.Width = 5;
-        rectangle.Height = 10;
-        Console.WriteLine("Area of rectangle: {0}", rectangle.CalculateArea());
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Rectangle rectangle = new Rectangle();
+//        rectangle.Width = 5;
+//        rectangle.Height = 10;
+//        Console.WriteLine("Area of rectangle: {0}", rectangle.CalculateArea());
 
-        Circle circle = new Circle();
-        circle.Radius = 7;
-        Console.WriteLine("Area of circle: {0}", circle.CalculateArea());
+//        Circle circle = new Circle();
+//        circle.Radius = 7;
+//        Console.WriteLine("Area of circle: {0}", circle.CalculateArea());
 
-        Console.ReadLine();
-    }
-}
+//        Console.ReadLine();
+//    }
+//}
--- StartProject1/MultipleInheritence.cs
+++ StartProject1/MultipleInheritence.cs
@@ -1,35 +1,35 @@
-﻿using System;
+﻿//using System;
 
-interface IShape
-{
-    void Draw();
-}
+//interface IShape
+//{
+//    void Draw();
+//}
 
-interface IColor
-{
-    void Colorize();
-}
+//interface IColor
+//{
+//    void Colorize();
+//}
 
-class Circle : IShape, IColor
-{
-    public void Draw()
-    {
-        Console.WriteLine("Circle is drawn.");
-    }
+//class Circle : IShape, IColor
+//{
+//    public void Draw()
+//    {
+//        Console.WriteLine("Circle is drawn.");
+//    }
 
-    public void Colorize()
-    {
-        Console.WriteLine("Circle is colorized.");
-    }
-}
+//    public void Colorize()
+//    {
+//        Console.WriteLine("Circle is colorized.");
+//    }
+//}
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Circle circle = new Circle();
-        circle.Draw();
-        circle.Colorize();
-        Console.ReadLine();
-    }
-}
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Circle circle = new Circle();
+//        circle.Draw();
+//        circle.Colorize();
+//        Console.ReadLine();
+//    }
+//}
