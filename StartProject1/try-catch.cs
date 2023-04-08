cf8de6a725e3657623d5f912a5d2baeaf9acec78
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 16:50:07 +0545

    add: try catch statements

---
+++ StartProject1/try-catch.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        try
+//        {
+//            int a = 10, b = 0;
+//            int c = a / b; // This line will throw a DivideByZeroException
+//        }
+//        catch (DivideByZeroException ex)
+//        {
+//            Console.WriteLine("Caught exception: " + ex.Message);
+//        }
+//        finally
+//        {
+//            Console.WriteLine("Finally block executed.");
+//        }
+
+//        Console.WriteLine("Program completed successfully.");
+//        Console.ReadLine();
+//    }
+//}
