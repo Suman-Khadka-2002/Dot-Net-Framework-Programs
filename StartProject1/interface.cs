da3531b9f56bef87c148255567ffc81920db104a
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 16:49:50 +0545

    add: interface

---
+++ StartProject1/interface.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+
+//public interface IShape
+//{
+//    double CalculateArea();
+//}
+
+//public class Circle : IShape
+//{
+//    private double radius;
+
+//    public Circle(double radius)
+//    {
+//        this.radius = radius;
+//    }
+
+//    public double CalculateArea()
+//    {
+//        return Math.PI * Math.Pow(radius, 2);
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        IShape shape = new Circle(5);
+//        Console.WriteLine("Area of circle: {0}", shape.CalculateArea());
+//        Console.ReadLine();
+//    }
+//}
