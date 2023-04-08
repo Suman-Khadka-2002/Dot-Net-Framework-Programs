c0089995729faa8b47170b7b30b5c880608257c7
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 15:17:56 +0545

    add: default constructor

---
+++ StartProject1/defaultConstructor.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+
+//class Person
+//{
+//    private string name;
+//    private int age;
+
+//    public Person()
+//    {
+//        name = "Kim Jisoo";
+//        age = 28;
+//    }
+
+//    public void SayHello()
+//    {
+//        Console.WriteLine("Hello, my name is " + name + " and I am " + age + " years old.");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Person person = new Person();
+//        person.SayHello();
+//        Console.ReadLine();
+//    }
+//}
