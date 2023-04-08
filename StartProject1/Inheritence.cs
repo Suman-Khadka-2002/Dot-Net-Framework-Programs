226129b1801df32e40045f56716126bea1b3f5b4
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 15:17:18 +0545

    add: Inheritence

---
+++ StartProject1/Inheritence.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+
+//class Animal
+//{
+//    private string name;
+
+//    public Animal(string name)
+//    {
+//        this.name = name;
+//    }
+
+//    public void Eat()
+//    {
+//        Console.WriteLine(name + " is eating.");
+//    }
+//}
+
+//class Cat : Animal
+//{
+//    public Cat(string name) : base(name)
+//    {
+//    }
+
+//    public void Meow()
+//    {
+//        Console.WriteLine("Meow!");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Cat cat = new Cat("Whiskers");
+//        cat.Eat(); // inherited from Animal class
+//        cat.Meow(); // specific to Cat class
+//        Console.ReadLine();
+//    }
+//}
