e102057f52504a2cc55d54f0db74d3553f3ce547
Suman Khadka <dnatsu022@gmail.com>
2023-04-08 15:51:24 +0545

    add: abstract method

---
+++ StartProject1/Abstract.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.Width = 5;
+        rectangle.Height = 10;
+        Console.WriteLine("Area of rectangle: {0}", rectangle.CalculateArea());
+
+        Circle circle = new Circle();
+        circle.Radius = 7;
+        Console.WriteLine("Area of circle: {0}", circle.CalculateArea());
+
+        Console.ReadLine();
+    }
+}
