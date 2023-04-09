de67a3b9a0d3fb00c4f036597cf1afaca546e7d6
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:07:10 +0545

    added: delegate

---
+++ StartProject1/delegate.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+
+//delegate void MyDelegate(string message);
+//delegate void MyDelegates();
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        MyDelegate myDelegate = new MyDelegate(PrintMessage);
+//        myDelegate("Hello, world!");
+
+//        MyDelegates myOtherDelegate = new MyDelegates(PrintGreeting);
+//        myOtherDelegate();
+
+//        Console.ReadLine();
+//    }
+
+//    static void PrintMessage(string message)
+//    {
+//        Console.WriteLine(message);
+//    }
+
+//    static void PrintGreeting()
+//    {
+//        Console.WriteLine("Greetings!");
+//    }
+//}
