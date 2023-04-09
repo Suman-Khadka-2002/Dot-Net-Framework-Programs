77f8366a852ce40f9a31fa6f9a054f4ba84aadb9
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:09:44 +0545

    added: lists

---
+++ StartProject1/list.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Create a new List of integers
+//        List<int> numbers = new List<int>();
+
+//        // Add some elements to the List
+//        numbers.Add(1);
+//        numbers.Add(2);
+//        numbers.Add(3);
+
+//        // Insert an element at a specific position
+//        numbers.Insert(1, 4);
+
+//        // Remove an element from the List
+//        numbers.Remove(2);
+
+//        // Print the contents of the List
+//        foreach (int number in numbers)
+//        {
+//            Console.WriteLine(number);
+//        }
+
+//        Console.ReadLine();
+//    }
+//}
