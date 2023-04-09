f9da717e3cb76a6022da189acd5b65a442825a8e
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:09:29 +0545

    added: hashtable

---
+++ StartProject1/hashtable.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Hashtable mylist = new Hashtable();
+
+//        mylist.Add("name", "suman");
+//        mylist.Add("age", "20");
+//        mylist.Add("College", "BMC");
+//        mylist.Add("mobile", "9874973");
+
+//        // Print the contents of the Hashtable
+//        Console.WriteLine("Hashtable contents:");
+//        foreach (DictionaryEntry item in mylist)
+//        {
+//            //Console.WriteLine("Key is " + item + " and value is " + mylist[item]);
+//            Console.WriteLine(item.Key + " : " + item.Value);
+//        }
+//        Console.WriteLine("\n");
+
+//        // Remove an element from the Hashtable
+//        mylist.Remove("mobile");
+
+//        // Print the contents of the Hashtable again
+//        Console.WriteLine("Hashtable contents after removing mobile:");
+//        foreach (DictionaryEntry item in mylist)
+//        {
+//            Console.WriteLine(item.Key + ": " + item.Value);
+//        }
+
+//        Console.ReadLine();
+//    }
+//}
