d78bdb1802c767b24db2e1ce0df2d8ccd85e5157
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:08:15 +0545

    added: arraylist

---
+++ StartProject1/arraylist.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        ArrayList mylist = new ArrayList();
+//        mylist.Add("BMC");
+//        mylist.Add(1234);
+//        mylist.Add("Ab-2");
+//        mylist.Add("hello cutie");
+        
+//        Console.WriteLine("Arraylist contents: ");
+//        foreach (var item in mylist)
+//        {
+//            Console.WriteLine(item);
+//        }
+//        Console.WriteLine("\n");
+
+//        // Remove an element from the ArrayList
+//        mylist.Remove("Ab-2");
+
+//        // Print the contents of the ArrayList again
+//        Console.WriteLine("ArrayList contents after removing Ab-2:");
+//        foreach (var item in mylist)
+//        {
+//            Console.WriteLine(item);
+//        }
+
+//        Console.ReadLine();
+//    }
+//}
