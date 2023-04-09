4776fd01e26d59c79d5a3468acc0af3e189d58bc
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:10:01 +0545

    added: sealed class

---
+++ StartProject1/sealedclass.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+
+//sealed class MyClass
+//{
+//    private string msg;
+
+//    public MyClass(string message)
+//    {
+//        msg = message;
+//    }
+
+//    public void SayHello()
+//    {
+//        Console.WriteLine(msg);
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        MyClass myObj = new MyClass("Hello from MyClass!");
+//        myObj.SayHello();
+//        Console.ReadLine();
+//    }
+//}
