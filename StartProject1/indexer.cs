98ade8c5f37220dd85e5bb291e018036291e61a1
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:20:58 +0545

    added: indexer

--- StartProject1/indexer.cs
+++ StartProject1/indexer.cs
@@ -1,31 +1,31 @@
-﻿using System;
+﻿//using System;
 
-public class MyCollection
-{
-    private string[] items = new string[5];
+//public class MyCollection
+//{
+//    private string[] items = new string[5];
 
-    public string this[int index]
-    {
-        get { return items[index]; }
-        set { items[index] = value; }
-    }
-}
+//    public string this[int index]
+//    {
+//        get { return items[index]; }
+//        set { items[index] = value; }
+//    }
+//}
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        MyCollection collection = new MyCollection();
-        collection[0] = "apple";
-        collection[1] = "banana";
-        collection[2] = "cherry";
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        MyCollection collection = new MyCollection();
+//        collection[0] = "apple";
+//        collection[1] = "banana";
+//        collection[2] = "cherry";
 
-        Console.WriteLine("Items in collection:");
-        for (int i = 0; i < 3; i++)
-        {
-            Console.WriteLine(collection[i]);
-        }
+//        Console.WriteLine("Items in collection:");
+//        for (int i = 0; i < 3; i++)
+//        {
+//            Console.WriteLine(collection[i]);
+//        }
 
-        Console.ReadLine();
-    }
-}
+//        Console.ReadLine();
+//    }
+//}
