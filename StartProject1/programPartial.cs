375f4798f28f6208b6ec6b84771c542d84d98ca2
Suman Khadka <dnatsu022@gmail.com>
2023-04-09 18:12:42 +0545

    added: partialclass main

---
+++ StartProject1/programPartial.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        MyClass myObject = new MyClass();
+//        myObject.Method1();
+//        myObject.Method2();
+
+//        Console.ReadLine();
+//    }
+//}
