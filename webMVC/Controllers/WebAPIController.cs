99fe726e9643af6452a0c65d1df32a3ca59415e7
Suman Khadka <dnatsu022@gmail.com>
2023-04-24 20:28:17 +0545

    added: webAPI, eng-nep time interface

---
+++ webMVC/Controllers/WebAPIController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebAPIController : ControllerBase
+    {
+        // GET: api/<WebAPIController>
+        [HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WebAPIController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WebAPIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WebAPIController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WebAPIController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
