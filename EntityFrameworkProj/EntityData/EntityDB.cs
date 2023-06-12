1cc0d7a3010893671422296afde54b30e23e1828
Suman Khadka <dnatsu022@gmail.com>
2023-06-12 22:40:46 +0545

    nested functions have access to enclosing function's variable scope

--- EntityFrameworkProj/EntityData/EntityDB.cs
+++ EntityFrameworkProj/EntityData/EntityDB.cs
@@ -1,6 +1,15 @@
-﻿namespace EntityFrameworkProj.EntityData
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkProj.EntityData
 {
-    public class EntityDB
+    public class EntityDB : DbContext
     {
+        public EntityDB(DbContextOptions<EntityDB> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
     }
 }
