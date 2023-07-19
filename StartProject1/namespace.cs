3c4f25d9428ef170bdbbdaef802242d84c024f78
Suman Khadka <dnatsu022@gmail.com>
2023-07-19 22:27:51 +0545

    introduction to namespace

---
+++ StartProject1/namespace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        public void MyMethod() 
+        {
+            Console.WriteLine("Hello from MyMethod.");
+            Console.ReadLine();
+        }
+
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        MyNamespace.MyClass obj1 = new MyNamespace.MyClass();
+        obj1.MyMethod();
+    }
+}
