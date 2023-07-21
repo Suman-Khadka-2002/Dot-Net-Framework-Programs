1c93283d762b09b62691207161f84213eff4a77f
Suman Khadka <dnatsu022@gmail.com>
2023-07-21 22:32:39 +0545

    delegate

--- StartProject1/delegate.cs
+++ StartProject1/delegate.cs
@@ -1,28 +1,28 @@
-﻿//using System;
+﻿using System;
 
-//delegate void MyDelegate(string message);
-//delegate void MyDelegates();
+delegate void MyDelegate(string message);
+delegate void MyDelegates();
 
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        MyDelegate myDelegate = new MyDelegate(PrintMessage);
-//        myDelegate("Hello, world!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDelegate myDelegate = new MyDelegate(PrintMessage);
+        myDelegate("Hello, Suman!");
 
-//        MyDelegates myOtherDelegate = new MyDelegates(PrintGreeting);
-//        myOtherDelegate();
+        MyDelegates myOtherDelegate = new MyDelegates(PrintGreeting);
+        myOtherDelegate();
 
-//        Console.ReadLine();
-//    }
+        Console.ReadLine();
+    }
 
-//    static void PrintMessage(string message)
-//    {
-//        Console.WriteLine(message);
-//    }
+    static void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
 
-//    static void PrintGreeting()
-//    {
-//        Console.WriteLine("Greetings!");
-//    }
-//}
+    static void PrintGreeting()
+    {
+        Console.WriteLine("Greetings!");
+    }
+}
