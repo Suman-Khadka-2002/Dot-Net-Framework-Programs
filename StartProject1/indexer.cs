930109919d291e00659bb1b4f056e6f4d7ce1564
Suman Khadka <dnatsu022@gmail.com>
2023-07-28 22:13:56 +0545

    indexer

--- StartProject1/indexer.cs
+++ StartProject1/indexer.cs
@@ -29,3 +29,29 @@
 //        Console.ReadLine();
 //    }
 //}
+
+using System;
+
+class SampleCollection<T>
+{
+    // Declare an array to store the data elements.
+    private T[] arr = new T[100];
+
+    // Define the indexer to allow client code to use [] notation.
+    public T this[int i]
+    {
+        get { return arr[i]; }
+        set { arr[i] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var stringCollection = new SampleCollection<string>();
+        stringCollection[0] = "Hello, World";
+        Console.WriteLine(stringCollection[0]);
+        Console.ReadLine();
+    }
+}
