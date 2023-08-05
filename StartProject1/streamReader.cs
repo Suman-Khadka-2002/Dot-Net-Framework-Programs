4c630946d8e550824b864710daae155ea4d078fe
Suman Khadka <dnatsu022@gmail.com>
2023-08-05 22:04:49 +0545

    modified

--- StartProject1/streamReader.cs
+++ StartProject1/streamReader.cs
@@ -1,19 +1,19 @@
 ﻿// File I/O : streamReader 
 
-using System;
-using System.IO;
-class Program
-{
-    static void Main()
-    {
-        // Open the file for reading
-        using (StreamReader reader = new StreamReader("E:\\. NET programs\\StartProject1\\output.txt"))
-        {
-            // Read the contents of the file and write them to the console
-            string contents = reader.ReadToEnd();
-            Console.WriteLine(contents);
-        }
-        Console.WriteLine("\nText is read.");
-        Console.ReadKey(); 
-    }
-}
+//using System;
+//using System.IO;
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Open the file for reading
+//        using (StreamReader reader = new StreamReader("E:\\. NET programs\\StartProject1\\output.txt"))
+//        {
+//            // Read the contents of the file and write them to the console
+//            string contents = reader.ReadToEnd();
+//            Console.WriteLine(contents);
+//        }
+//        Console.WriteLine("\nText is read.");
+//        Console.ReadKey(); 
+//    }
+//}
