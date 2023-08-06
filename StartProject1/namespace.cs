9cc1518ceb0f234bc1f42453d947774f0c327883
Suman Khadka <dnatsu022@gmail.com>
2023-08-06 22:09:47 +0545

    modified

--- StartProject1/namespace.cs
+++ StartProject1/namespace.cs
@@ -1,21 +1,21 @@
-﻿using System;
-namespace MyNamespace
-{
-    public class MyClass
-    {
-        public void MyMethod() 
-        {
-            Console.WriteLine("Hello from MyMethod.");
-            Console.ReadLine();
-        }
+﻿//using System;
+//namespace MyNamespace
+//{
+//    public class MyClass
+//    {
+//        public void MyMethod() 
+//        {
+//            Console.WriteLine("Hello from MyMethod.");
+//            Console.ReadLine();
+//        }
 
-    }
-}
-class Program
-{
-    public static void Main()
-    {
-        MyNamespace.MyClass obj1 = new MyNamespace.MyClass();
-        obj1.MyMethod();
-    }
-}
+//    }
+//}
+//class Program
+//{
+//    public static void Main()
+//    {
+//        MyNamespace.MyClass obj1 = new MyNamespace.MyClass();
+//        obj1.MyMethod();
+//    }
+//}
